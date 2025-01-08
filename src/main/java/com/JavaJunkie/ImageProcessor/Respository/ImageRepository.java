1363eec6712f84ab2641e3c83794591137359ed5
AyushInKC <sirfayush1008@gmail.com>
2025-01-08 23:33:17 +0530

    Connected with the Atlas

--- src/main/java/com/JavaJunkie/ImageProcessor/Respository/ImageRepository.java
+++ src/main/java/com/JavaJunkie/ImageProcessor/Respository/ImageRepository.java
@@ -1,6 +1,9 @@
 package com.JavaJunkie.ImageProcessor.Respository;
 import com.JavaJunkie.ImageProcessor.Entity.ImageEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ImageRepository extends MongoRepository<ImageEntity,String> {
 
 }
--- src/main/java/com/JavaJunkie/ImageProcessor/Services/ImageService.java
+++ src/main/java/com/JavaJunkie/ImageProcessor/Services/ImageService.java
@@ -32,7 +32,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
-@Component
 @Service
 public class ImageService {
     @Autowired
